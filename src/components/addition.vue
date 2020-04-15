47cc6823cf4fb6094a7860da438d251af05984cf
tian7712 <tian7712@gmail.com>
2020-04-15 13:54:02 +0800

    更新的文件

--- src/components/addition.vue
+++ src/components/addition.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>当前最新的count值为:</h1>
+    <h1>当前最新的count值为:{{$store.state.count}}</h1>
     <button>+1</button>
   </div>
 </template>
--- src/components/subrtaction.vue
+++ src/components/subrtaction.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <h1>当前最新的count值为:</h1>
+    <h1>当前最新的count值为:{{count}}</h1>
     <button>-1</button>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 export default {
   data() {
     return {}
+  },
+  computed: {
+      ...mapState(['count'])
   }
 }
 </script>
