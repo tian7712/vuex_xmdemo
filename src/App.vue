ad5700bd000811fb23407e8c0412c5fdfc2e4e7c
tian7712 <tian7712@gmail.com>
2020-04-15 12:22:40 +0800

    初始化计数器

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-appition></my-appition>
+    <p>------------------------</p>
+    <my-subrtaction></my-subrtaction>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Appition from './components/addition';
+import Subrtaction from './components/subrtaction';
 export default {
-  name: 'App',
+  data() {
+    return {}
+  },
   components: {
-    HelloWorld
+    'my-appition': Appition,
+    'my-subrtaction': Subrtaction
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/addition.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>当前最新的count值为:</h1>
+    <button>+1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+};
+</script>
